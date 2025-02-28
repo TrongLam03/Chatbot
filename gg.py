8014efe5a0585889b48593a450c9a28a8ed3e10f
TrongLam03 <lamnguyentrong3@gmail.com>
2025-02-28 18:11:38 +0700

    Update gg.py

--- gg.py
+++ gg.py
@@ -1,15 +1,20 @@
-import google.generativeai as palm
+import openai
+API_KEY = open(API_KEY ,"  ").read()
+openai.api.key= Your API...
 
-# Khởi tạo với API Key của bạn
-palm.configure(api_key="AIzaSyDjI-t8qy06KGh2d46TmHTen3S24hB35jE")
+chat_log=[]
 
-# Ví dụ về việc tạo phản hồi từ chat bot
-def chat_with_palm(prompt):
-    response = palm.chat(prompt)
-    return response
-
-# Ví dụ sử dụng
-if __name__ == "__main__":
-    prompt = "Hi, how can I assist you today?"
-    response = chat_with_palm(prompt)
-    print("Bot:", response)
+while True:
+    user_messeges=input("Messages")
+    if user_messages.lower()== "quit":
+        break
+    else:]
+        chat.log.append({"role": "user": "content": user_message})
+        respone = openai.ChatCompeletion.create(
+            model= " your model"
+            messages= chat_log
+        )
+        assistant_respone= respone['choice'][0]['messages']['content']
+        print("CHAT ", assistant_response.strip("/n)
+        chat.log.append({"role": "assistant","content": assistant_response.strip("/n").strip()})
+print(respone)
